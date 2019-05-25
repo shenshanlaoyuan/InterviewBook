7f5961166d629f51103da3c2aafbc34d5af5b205
shenshanlaoyuan <shenshanlaoyuan@gmail.com>
2019-05-25 17:31:41 +0800

    修改评论插件

--- node_modules/gitbook-plugin-tbfed-pagefooter/gitalk-config.js
+++ node_modules/gitbook-plugin-tbfed-pagefooter/gitalk-config.js
@@ -4,7 +4,7 @@
 var gitalk = new Gitalk({
     "clientID": "941df607ad0d8a39b204",
     "clientSecret": "bd5e6cd6336fcebff410676a7c5d41235030a2f7",
-    "repo": "https://github.com/shenshanlaoyuan/InterviewBook",
+    "repo": "InterviewBook",
     "owner": "shenshanlaoyuan",
     "admin": ["shenshanlaoyuan"],
     "id": location.pathname,
