a6d8121b61bedbd14cdc02a2e307776aaa10b492
shenshanlaoyuan <shenshanlaoyuan@gmail.com>
2019-05-29 15:37:58 +0800

    修改readme添加目录

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,13 +1,15 @@
 /**
  * Created by shenshanlaoyuan on 2019/5/25.
  */
+ <script src="js/md5.min.js"></script>
+
 var gitalk = new Gitalk({
     "clientID": "941df607ad0d8a39b204",
     "clientSecret": "bd5e6cd6336fcebff410676a7c5d41235030a2f7",
     "repo": "InterviewBook",
     "owner": "shenshanlaoyuan",
     "admin": ["shenshanlaoyuan"],
-    "id": decodeURI(location.pathname),
+    "id": md5(location.pathname),
     "distractionFreeMode": false
 });
 gitalk.render("gitalk-container");
